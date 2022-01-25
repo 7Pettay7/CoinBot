9ec45e186676f3a493181a2f74091680a7ccbcfc
7Pettay7 <benpettay@gmail.com>
2022-01-25 14:01:38 -0500

    cleaned up controller.py

--- coinbot/controller.py
+++ coinbot/controller.py
@@ -1,27 +1,22 @@
-import json
-import requests
-
 from crypto import Portfolio as folio
 from auth import Authorize as auth
 
 class Controller:
-    """Controller class used to pass and format data to bot from crypto module
-    
-    TODO: cleanup unneeded self variables"""
+    """Controller class used to pass and format data to bot from crypto module"""
 
     def __init__(self, secrets):
         self.login = folio(secrets)
 
     def portfolio_data(self):
-        self.balances = self.login.sort_dict(
+        balances = self.login.sort_dict(
             self.login.get_balances())
-        self.total = self.login.get_total(self.balances)
-        return [self.balances, self.total]
+        total = self.login.get_total(balances)
+        return [balances, total]
 
     def current_data(self):
-        self.current_prices = self.login.current_prices(
+        current_prices = self.login.current_prices(
             self.login.get_balances())
-        return self.current_prices
+        return current_prices
 
     def historic_data(self):
         balances = self.portfolio_data()[0]
